68a868f998b3e38a446a88da6ed12c11c41f1b31
Bruno VIttor <bv.everit@itmss.com.br>
2020-08-29 12:23:56 -0300

    CONVERSOR DE DÓLAR

---
+++ ex011.py
@@ -0,0 +1,8 @@
+# faça um programa que leia a altura e a largura de uma parede,
+# calcule sua area e quanto de tinta será nescessario para pintar a parede
+
+al = float(input('Digite a altura da parede: '))
+la = float(input('digite a largura da parede: '))
+ar = al * la
+lts = ar / 2
+print('A área total da parede é de {}m² e serão nescessários {} lts de tinta'.format(ar, lts))
